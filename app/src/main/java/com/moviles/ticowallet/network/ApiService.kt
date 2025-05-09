45b9fcf83e874ef336a2dd4f45cd7ad6480ebe0b
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-05-09 12:56:00 -0600

    Added user model, apiservice, constants and retrofit

---
+++ app/src/main/java/com/moviles/ticowallet/network/ApiService.kt
@@ -0,0 +1,31 @@
+package com.moviles.ticowallet.network
+
+import com.moviles.ticowallet.models.User
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.Path
+
+interface ApiService {
+//    USERS
+
+    @Multipart
+    @POST("api/user/register")
+    suspend fun addUser(@Part("Name") name: RequestBody,
+                         @Part("Password") location: RequestBody,
+                         @Part("Email") description: RequestBody,
+                         @Part("Date") date: RequestBody,
+                         @Part file: MultipartBody.Part?): User
+
+
+
+
+//    Goals
+}
